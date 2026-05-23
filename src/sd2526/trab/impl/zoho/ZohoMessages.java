528626b8175d24216b991a3a753576804b9392a9
duarte-Win <duartecruzcoelho@gmail.com>
2026-05-23 16:24:22 +0100

    FAIL SAFE : passed tests 101a, 102, 103, 105[a,b,e], 106a, 107, 108. The rest were skipped

--- src/sd2526/trab/impl/zoho/ZohoMessages.java
+++ src/sd2526/trab/impl/zoho/ZohoMessages.java
@@ -9,7 +9,6 @@ import static sd2526.trab.api.java.Result.ErrorCode.NOT_FOUND;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
